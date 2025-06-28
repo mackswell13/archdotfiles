2664ff899f7791bc870368a5cbdc4cc4ce646631
Max Sherwin <maxwellsherwin@proton.me>
2025-06-28 16:02:21 -0500

    Fix tailwind ls being annoying

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,56 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                vim.lsp.protocol.make_client_capabilities(),
+                cmp_nvim_lsp.default_capabilities()
+            )
+
+            local lspconfig = require("lspconfig")
+
+            lspconfig.tailwindcss.setup({
+                capabilities = capabilities,
+                root_dir = require("lspconfig.util").root_pattern(
+                    "tailwind.config.js",
+                    "tailwind.config.ts",
+                    "postcss.config.js",
+                    "package.json",
+                    "Gemfile",
+                    ".git"
+                ),
+            })
+
+            lspconfig.ruby_lsp.setup({
+                capabilities = capabilities,
+                auto_start = true,
+                cmd = { "mise", "exec", "ruby", "--", "ruby-lsp" }
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
+            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+        end,
+    },
+}
